7d9641ef8f424686def33351c154d03f0c9e51d0
Maksim Shila <maksim.shila@gmail.com>
2022-01-15 16:28:36 +0300

    Added error handnling

--- JS/jbook/src/bundler/index.ts
+++ JS/jbook/src/bundler/index.ts
@@ -3,25 +3,29 @@ import { unpkgPathPlugin } from './plugins/unpckg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
 let service: esbuild.Service;
-const bundle = async (rawCode: string): Promise<string> => {
+const bundle = async (rawCode: string) => {
   if (!service) {
     service = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   }
-  const result = await service.build({
-    entryPoints: ['index.js'],
-    bundle: true,
-    write: false,
-    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
-    define: {
-      'process.env.NODE_ENV': '"production"',
-      global: 'window',
-    },
-  });
+  try {
+    const result = await service.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-  return result.outputFiles[0].text;
+    return { code: result.outputFiles[0].text, error: '' };
+  } catch (error: any) {
+    return { code: '', error: error.message };
+  }
 };
 
 export default bundle;
--- JS/jbook/src/components/code-cell.tsx
+++ JS/jbook/src/components/code-cell.tsx
@@ -7,12 +7,14 @@ import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [bundleError, setBundleError] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setBundleError(output.error);
     }, 500);
     return () => clearTimeout(timer);
   }, [input]);
@@ -26,7 +28,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} bundleError={bundleError} />
       </div>
     </Resizable>
   );
